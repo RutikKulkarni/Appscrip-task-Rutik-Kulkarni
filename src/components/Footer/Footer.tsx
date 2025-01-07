3026b4fe59fea6714ea94d315a58ca5f86587d4c
Rutik <rutikkulkarni2001@gmail.com>
2025-01-08 00:05:54 +0530

    Design the Footer & Navbar

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,56 +1,111 @@
-import styles from "./Footer.module.css";
+"use client";
 
-export default function Footer() {
+import React from "react";
+import styles from "./Footer.module.css";
+import { FaInstagram, FaLinkedin } from "react-icons/fa";
+import {
+  FaGooglePay,
+  FaCcMastercard,
+  FaCcAmex,
+  FaApplePay,
+} from "react-icons/fa";
+import { SiOpencollective } from "react-icons/si";
+
+const Footer = () => {
   return (
     <footer className={styles.footer}>
-      <div className={styles.footerContent}>
-        <div className={styles.socialLinks}>
-          <a
-            href="#"
-            className={styles.socialLink}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <i className="fa fa-facebook"></i>{" "}
-            {/* You can replace with icons */}
-          </a>
-          <a
-            href="#"
-            className={styles.socialLink}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <i className="fa fa-twitter"></i>
-          </a>
-          <a
-            href="#"
-            className={styles.socialLink}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <i className="fa fa-instagram"></i>
-          </a>
+      <div className={styles.topSection}>
+        <div className={styles.newsletter}>
+          <h4>BE THE FIRST TO KNOW</h4>
+          <p>Sign up for updates from metta muse.</p>
+          <div className={styles.inputWrapper}>
+            <input
+              type="email"
+              placeholder="Enter your e-mail..."
+              className={styles.input}
+            />
+            <button className={styles.subscribeButton}>SUBSCRIBE</button>
+          </div>
         </div>
-
-        <div className={styles.footerLinks}>
-          <a href="/privacy-policy" className={styles.footerLink}>
-            Privacy Policy
-          </a>
-          <a href="/terms-of-service" className={styles.footerLink}>
-            Terms of Service
-          </a>
-          <a href="/contact" className={styles.footerLink}>
-            Contact Us
-          </a>
-        </div>
-
-        <div className={styles.footerBottom}>
+        <div className={styles.contact}>
+          <h4>CONTACT US</h4>
+          <p>+44 221 133 5360</p>
+          <p>customercare@mettamuse.com</p>
+          <h4 style={{ marginTop: "2rem" }}>CURRENCY</h4>
+          <p>🇺🇸 USD</p>
           <p>
-            &copy; {new Date().getFullYear()} Appscrip-task. All rights
-            reserved.
+            Transactions will be completed in Euros and a currency reference is
+            available on hover.
           </p>
         </div>
       </div>
+
+      <hr className={styles.divider} />
+
+      <div className={styles.bottomSection}>
+        <div className={styles.links}>
+          <div className={styles.column}>
+            <h4>metta muse</h4>
+            <ul>
+              <li>About Us</li>
+              <li>Stories</li>
+              <li>Artisans</li>
+              <li>Boutiques</li>
+              <li>Contact Us</li>
+              <li>EU Compliances Docs</li>
+            </ul>
+          </div>
+          <div className={styles.column}>
+            <h4>QUICK LINKS</h4>
+            <ul>
+              <li>Orders & Shipping</li>
+              <li>Join/Login as a Seller</li>
+              <li>Payment & Pricing</li>
+              <li>Return & Refunds</li>
+              <li>FAQs</li>
+              <li>Privacy Policy</li>
+              <li>Terms & Conditions</li>
+            </ul>
+          </div>
+          <div className={styles.column}>
+            <h4>FOLLOW US</h4>
+            <div className={styles.socialIcons}>
+              <FaInstagram />
+              <FaLinkedin />
+            </div>
+            <h4 style={{ marginTop: "2rem" }}>metta muse ACCEPTS</h4>
+            <div className={styles.paymentIcons}>
+              <FaGooglePay
+                size={40}
+                className="text-black"
+                title="Google Pay"
+              />
+              <FaCcMastercard
+                size={40}
+                className="text-red-500"
+                title="MasterCard"
+              />
+              <FaCcAmex
+                size={40}
+                className="text-blue-600"
+                title="American Express"
+              />
+              <FaApplePay size={40} className="text-black" title="Apple Pay" />
+              <SiOpencollective
+                size={40}
+                className="text-green-500"
+                title="Opay"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <p className={styles.copyright}>
+        Copyright © 2023 mettamuse. All rights reserved.
+      </p>
     </footer>
   );
-}
+};
+
+export default Footer;
